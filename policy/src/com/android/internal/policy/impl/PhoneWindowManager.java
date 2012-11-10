2a9fc66824848c41d60a7bae3d9ed7342c2bcc4c
rohan <rohanmathur34@gmail.com>
2012-11-10 13:34:27 -0500

    Revert "PWM: Fix launch of home button when screen times out"
    Needed revert for hardware keys commit.
    Commit doesn't solve a major bug anyway.
    
    This reverts commit 7c289f5387e651b1736abc8466b4a83b4a66f578.

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2155,7 +2155,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             // If we have released the home key, and didn't do anything else
             // while it was pressed, then it is time to go home!
-            if (!down && mHomePressed) {
+            if (!down) {
                 final boolean homeWasLongPressed = mHomeLongPressed;
                 mHomeLongPressed = false;
                 if (!homeWasLongPressed) {
@@ -2163,6 +2163,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         cancelPreloadRecentApps();
                     }
 
+                    mHomePressed = false;
                     if (!canceled) {
                         // If an incoming call is ringing, HOME is totally disabled.
                         // (The user is already on the InCallScreen at this point,
